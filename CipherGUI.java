7227e823495e24828916fd8c898690c68fa081a5
elruds <32840432+elruds@users.noreply.github.com>
2017-11-11 16:56:18 +0000

    Add files via upload

---
+++ CipherGUI.java
@@ -0,0 +1,130 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.io.*;
+
+/** 
+ * Programming AE2
+ * Class to display cipher GUI and listen for events
+ */
+public class CipherGUI extends JFrame implements ActionListener
+{
+	//instance variables which are the components
+	private JPanel top, bottom, middle;
+	private JButton monoButton, vigenereButton;
+	private JTextField keyField, messageField;
+	private JLabel keyLabel, messageLabel;
+
+	//application instance variables
+	//including the 'core' part of the textfile filename
+	//some way of indicating whether encoding or decoding is to be done
+	private MonoCipher mcipher;
+	private VCipher vcipher;
+	
+	/**
+	 * The constructor adds all the components to the frame
+	 */
+	public CipherGUI()
+	{
+		this.setSize(400,150);
+		this.setLocation(100,100);
+		this.setTitle("Cipher GUI");
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.layoutComponents();
+	}
+	
+	/**
+	 * Helper method to add components to the frame
+	 */
+	public void layoutComponents()
+	{
+		//top panel is yellow and contains a text field of 10 characters
+		top = new JPanel();
+		top.setBackground(Color.yellow);
+		keyLabel = new JLabel("Keyword : ");
+		top.add(keyLabel);
+		keyField = new JTextField(10);
+		top.add(keyField);
+		this.add(top,BorderLayout.NORTH);
+		
+		//middle panel is yellow and contains a text field of 10 characters
+		middle = new JPanel();
+		middle.setBackground(Color.yellow);
+		messageLabel = new JLabel("Message file : ");
+		middle.add(messageLabel);
+		messageField = new JTextField(10);
+		middle.add(messageField);
+		this.add(middle,BorderLayout.CENTER);
+		
+		//bottom panel is green and contains 2 buttons
+		
+		bottom = new JPanel();
+		bottom.setBackground(Color.green);
+		//create mono button and add it to the top panel
+		monoButton = new JButton("Process Mono Cipher");
+		monoButton.addActionListener(this);
+		bottom.add(monoButton);
+		//create vigenere button and add it to the top panel
+		vigenereButton = new JButton("Process Vigenere Cipher");
+		vigenereButton.addActionListener(this);
+		bottom.add(vigenereButton);
+		//add the top panel
+		this.add(bottom,BorderLayout.SOUTH);
+	}
+	
+	/**
+	 * Listen for and react to button press events
+	 * (use helper methods below)
+	 * @param e the event
+	 */
+	public void actionPerformed(ActionEvent e)
+	{	
+		if (e.getSource() == monoButton || e.getSource() == vigenereButton) {
+		getKeyword();
+		processFileName();	
+	}	
+	}	
+		
+	    // your code
+	
+	
+	/** 
+	 * Obtains cipher keyword
+	 * If the keyword is invalid, a message is produced
+	 * @return whether a valid keyword was entered
+	 */
+	private boolean getKeyword()
+	{
+	  String keyword = keyField.getText();
+	  
+	  if (keyword == "")
+		
+		
+	  
+	    return true;  // replace with your code
+	}
+	
+	/** 
+	 * Obtains filename from GUI
+	 * The details of the filename and the type of coding are extracted
+	 * If the filename is invalid, a message is produced 
+	 * The details obtained from the filename must be remembered
+	 * @return whether a valid filename was entered
+	 */
+	private boolean processFileName()
+	{
+	    return true;  // replace with your code
+	}
+	
+	/** 
+	 * Reads the input text file character by character
+	 * Each character is encoded or decoded as appropriate
+	 * and written to the output text file
+	 * @param vigenere whether the encoding is Vigenere (true) or Mono (false)
+	 * @return whether the I/O operations were successful
+	 */
+	private boolean processFile(boolean vigenere)
+	{
+	    return true;  // replace with your code
+	}
+}
---
+++ MonoCipher.java
@@ -0,0 +1,51 @@
+/**
+ * Programming AE2
+ * Contains monoalphabetic cipher and methods to encode and decode a character.
+ */
+public class MonoCipher
+{
+	/** The size of the alphabet. */
+	private final int SIZE = 26;
+
+	/** The alphabet. */
+	private char [] alphabet;
+	
+	/** The cipher array. */
+	private char [] cipher;
+
+	/**
+	 * Instantiates a new mono cipher.
+	 * @param keyword the cipher keyword
+	 */
+	public MonoCipher(String keyword)
+	{
+		//create alphabet
+		alphabet = new char [SIZE];
+		for (int i = 0; i < SIZE; i++)
+			alphabet[i] = (char)('A' + i);
+		
+		// create first part of cipher from keyword
+		// create remainder of cipher from the remaining characters of the alphabet
+		// print cipher array for testing and tutors
+	}
+	
+	/**
+	 * Encode a character
+	 * @param ch the character to be encoded
+	 * @return the encoded character
+	 */
+	public char encode(char ch)
+	{
+	    return ' ';  // replace with your code
+	}
+
+	/**
+	 * Decode a character
+	 * @param ch the character to be encoded
+	 * @return the decoded character
+	 */
+	public char decode(char ch)
+	{
+	    return ' ';  // replace with your code
+	}
+}
